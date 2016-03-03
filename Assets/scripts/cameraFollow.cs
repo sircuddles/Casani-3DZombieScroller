2ca67cb5766b3f9931ec41513a46e0896921d51e
sircuddles <governmentslave@hotmail.com>
2016-03-03 15:02:02 -0500

    E9 - Add dynamic camera, fixed landing animation

---
+++ Assets/scripts/cameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothing = 5f;
+    [SerializeField] private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 targetCamPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, Time.deltaTime * smoothing);
+	}
+}
