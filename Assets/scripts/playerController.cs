5e0a612bd0691b4d190cd801d0f9ad3672c11f5b
sircuddles <governmentslave@hotmail.com>
2016-03-03 14:46:19 -0500

    E7-8 - Add jumping and animations

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -10,6 +10,14 @@ public class playerController : MonoBehaviour {
 
     private bool facingRight;
 
+    // Jumping
+    private bool grounded = false;
+    private Collider[] groundCollisions;
+    private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float jumpHeight;
+
 	// Use this for initialization
 	void Start () {
         mRigid = GetComponent<Rigidbody>();
@@ -19,21 +27,39 @@ public class playerController : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        // Handle jump input and animation 
+        if (grounded && Input.GetAxis("Jump") > 0) {
+            grounded = false;
+            mAnimator.SetBool("grounded", grounded);
+            mRigid.AddForce(new Vector3(0, jumpHeight, 0));
+        }
+        
+        // Ground collision
+        groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
+        if (groundCollisions.Length > 0) grounded = true;
+        else grounded = false;
+
+        mAnimator.SetBool("grounded", grounded);
+        mAnimator.SetFloat("verticalSpeed", mRigid.velocity.y);
+
+        // Handle Walk/Running 
         float horizontalMove = Input.GetAxis("Horizontal");
         mAnimator.SetFloat("speed", Mathf.Abs(horizontalMove));
 
+        // Handle Sneaking
         float sneaking = Input.GetAxisRaw("Fire3");
         mAnimator.SetFloat("sneaking", sneaking);
 
-        if (sneaking > 0) {
+        // Move player
+        if (grounded && sneaking > 0) {
             mRigid.velocity = new Vector3(horizontalMove * walkSpeed, mRigid.velocity.y, 0);
         }
         else {
             mRigid.velocity = new Vector3(horizontalMove * runSpeed, mRigid.velocity.y, 0);
         }
 
-
+        // Flip player if necessary
         if (horizontalMove > 0 && !facingRight)
             Flip();
         else if (horizontalMove < 0 && facingRight)
