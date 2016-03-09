9e37954fd5a0b1148825a1335e31db9d35b8b33b
sircuddles <governmentslave@hotmail.com>
2016-03-09 11:02:49 -0500

    E14 - Add death void cleaner

---
+++ Assets/scripts/cleaner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cleaner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            playerHealth playerDead = other.gameObject.GetComponent<playerHealth>();
+            playerDead.makeDead();
+        }
+        else Destroy(other);
+    }
+}
