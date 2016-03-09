48f4891470aa4a534d07f83c6b82c24925c306bc
sircuddles <governmentslave@hotmail.com>
2016-03-09 10:52:08 -0500

    E13 - Add player and spike trap damage and death FX

---
+++ Assets/enemyDamage.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyDamage : MonoBehaviour {
+    public float damage;
+    public float damageRate;
+    public float pushBackForce;
+
+    float nextDamage;
+
+    private bool playerInRange = false;
+
+    GameObject thePlayer;
+    playerHealth thePlayerHealth;
+
+	// Use this for initialization
+	void Start () {
+        nextDamage = Time.time;
+
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+        thePlayerHealth = thePlayer.GetComponent<playerHealth>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (playerInRange) {
+            Attack();
+        }
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            playerInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "Player") {
+            playerInRange = false;
+        }
+    }
+
+    void Attack() {
+        if (nextDamage <= Time.time) {
+            thePlayerHealth.addDamage(damage);
+            nextDamage = Time.time + damageRate;
+            pushBack(thePlayer.transform);
+        }
+    }
+
+    void pushBack(Transform t) {
+        Vector3 pushDir = new Vector3(0, (t.position.y - transform.position.y), 0).normalized;
+        pushDir *= pushBackForce;
+
+        Rigidbody pushedRB = t.GetComponent<Rigidbody>();
+        pushedRB.velocity = Vector3.zero;
+        pushedRB.AddForce(pushDir, ForceMode.Impulse);
+    }
+}
---
+++ Assets/scripts/playerHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerHealth : MonoBehaviour {
+    public float maxHealth;
+    public float currentHealth;
+
+    public GameObject playerDeathFX;
+
+    void Start() {
+        currentHealth = maxHealth;
+    }
+
+    public void addDamage(float dmg) {
+        currentHealth -= dmg;
+
+        if (currentHealth <= 0) {
+            makeDead();
+        }
+    }
+
+    public void makeDead() {
+        Instantiate(playerDeathFX, transform.position, Quaternion.Euler(new Vector3(-90, 0, 0)));
+        Destroy(gameObject);
+
+    }
+}
