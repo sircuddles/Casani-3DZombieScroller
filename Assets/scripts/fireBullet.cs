d23f648348a31286b3a15b0d42a123ffd42df4b7
sircuddles <governmentslave@hotmail.com>
2016-03-10 11:23:34 -0500

    E21 - Add ammo slider and functionality to AR

--- Assets/scripts/fireBullet.cs
+++ Assets/scripts/fireBullet.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class fireBullet : MonoBehaviour {
     [SerializeField] private float timeBetweenBullets;
@@ -7,15 +8,24 @@ public class fireBullet : MonoBehaviour {
     private float nextBullet;
     private playerController myPC;
 
+    public int maxRounds;
+    public int startRounds;
+    int currentRounds;
+    public Slider playerAmmoSlider;
+
     // Use this for initialization
     void Awake () {
         nextBullet = 0f;
         myPC = transform.root.GetComponent<playerController>();
+
+        currentRounds = startRounds;
+        playerAmmoSlider.maxValue = maxRounds;
+        playerAmmoSlider.value = currentRounds;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetAxisRaw("Fire1") > 0 && nextBullet < Time.time) {
+	    if (Input.GetAxisRaw("Fire1") > 0 && nextBullet < Time.time && currentRounds > 0) {
             nextBullet = Time.time + timeBetweenBullets;
             Vector3 rot;
 
@@ -24,6 +34,8 @@ public class fireBullet : MonoBehaviour {
             else rot = new Vector3(0, 90, 0);
 
             Instantiate(projectile, transform.position, Quaternion.Euler(rot));
+            currentRounds--;
+            playerAmmoSlider.value = currentRounds;
         }
 	}
 }
