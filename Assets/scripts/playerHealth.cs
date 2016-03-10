d606d94088c47208d01f2e15ae15d87e4583ec88
sircuddles <governmentslave@hotmail.com>
2016-03-10 10:54:53 -0500

    E20 - Add player health code and damage overlay

--- Assets/scripts/playerHealth.cs
+++ Assets/scripts/playerHealth.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class playerHealth : MonoBehaviour {
     public float maxHealth;
@@ -7,12 +8,36 @@ public class playerHealth : MonoBehaviour {
 
     public GameObject playerDeathFX;
 
+    //HUD
+    public Slider playerHealthSlider;
+    public Image damageOverlay;
+    Color flashColor = new Color(255f, 255f, 255f, 1f);
+    float flashSpeed = 5f;
+    bool damaged = false;
+    
+
     void Start() {
         currentHealth = maxHealth;
+        playerHealthSlider.maxValue = maxHealth;
+        playerHealthSlider.value = currentHealth;
+        playerHealthSlider.minValue = 0;
+    }
+
+    void Update() {
+        if (damaged) {
+            damageOverlay.color = flashColor;
+        }
+        else {
+            damageOverlay.color = Color.Lerp(damageOverlay.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        damaged = false;
     }
 
     public void addDamage(float dmg) {
         currentHealth -= dmg;
+        playerHealthSlider.value = currentHealth;
+        damaged = true;
 
         if (currentHealth <= 0) {
             makeDead();
@@ -20,6 +45,7 @@ public class playerHealth : MonoBehaviour {
     }
 
     public void makeDead() {
+        damageOverlay.color = flashColor;
         Instantiate(playerDeathFX, transform.position, Quaternion.Euler(new Vector3(-90, 0, 0)));
         Destroy(gameObject);
 
