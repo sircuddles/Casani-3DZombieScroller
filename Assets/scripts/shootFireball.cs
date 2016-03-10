cebfc3b2c32ee6c187388050f1953183eb068c3f
sircuddles <governmentslave@hotmail.com>
2016-03-10 10:05:45 -0500

    E18 - Add fireball projectile and effect

---
+++ Assets/scripts/shootFireball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shootFireball : MonoBehaviour {
+    public float damage;
+    public float speed;
+
+    private Rigidbody mRigid;
+
+	// Use this for initialization
+	void Start () {
+        mRigid = GetComponentInParent<Rigidbody>();
+
+        // Set the correct direction for the projectile
+        if (transform.rotation.y > 0) mRigid.AddForce(Vector3.right * speed, ForceMode.Impulse);
+        else mRigid.AddForce(Vector3.left * speed, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Shootable")) {
+            mRigid.velocity = Vector3.zero;
+            Destroy(gameObject);
+        }
+    }
+}
