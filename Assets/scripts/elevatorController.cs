db32f5acee7079468ba6d9833cd34a6c01ccf253
sircuddles <governmentslave@hotmail.com>
2016-03-11 09:47:02 -0500

    E26 - Added functional elevator

---
+++ Assets/scripts/elevatorController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class elevatorController : MonoBehaviour {
+    public float resetTime;
+    float downtime;
+    Animator mAnim;
+    AudioSource eleAS;
+
+    bool elevIsUp = false;
+    
+
+	// Use this for initialization
+	void Start () {
+        mAnim = GetComponent<Animator>();
+        eleAS = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (downtime <= Time.time && elevIsUp) {
+            mAnim.SetTrigger("activateElevator");
+            elevIsUp = false;
+            eleAS.Play();
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            mAnim.SetTrigger("activateElevator");
+            downtime = Time.time + resetTime;
+            elevIsUp = true;
+            eleAS.Play();
+        }
+    }
+}
