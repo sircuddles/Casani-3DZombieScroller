0c125236e697514150ba912e6a27abd9758c8565
sircuddles <governmentslave@hotmail.com>
2016-03-11 10:32:27 -0500

    E27 - Add basic zombie character and skin randomization

---
+++ Assets/scripts/randomZombieAppearance.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomZombieAppearance : MonoBehaviour {
+    public Material[] skins;
+
+	// Use this for initialization
+	void Start () {
+        SkinnedMeshRenderer myRend = GetComponent<SkinnedMeshRenderer>();
+        myRend.material = skins[Random.Range(0, skins.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
